56abef6aab1687613cec532aa7fda3b711c5d8af
thienhanbaro <lethiennhanvip.vn@gmail.com>
2026-04-19 21:16:36 +0700

    Update HeaderAPI.h

--- HeaderAPI.h
+++ HeaderAPI.h
@@ -5,6 +5,6 @@
 
 static NSString *const kBaseAPIURL = @"http://qtrongxthnhan.duckdns.org/api/connect.php";
 
-static NSString *const kPackageToken = @"6560cac629e88181bd85310911de1f8c"; 
+static NSString *const kPackageToken = @"c5dacfc778e106e0fb87aa26fe3b77bd"; 
 
 #endif
