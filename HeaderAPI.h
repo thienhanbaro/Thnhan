b9e50fc62588fe6d9d644e69984ea97c474c450b
thienhanbaro <lethiennhanvip.vn@gmail.com>
2026-04-19 20:38:43 +0700

    Create HeaderAPI.h

---
+++ HeaderAPI.h
@@ -0,0 +1,10 @@
+#ifndef HeaderAPI_h
+#define HeaderAPI_h
+
+#import <Foundation/Foundation.h>
+
+static NSString *const kBaseAPIURL = @"http://qtrongxthnhan.duckdns.org/api/connect.php";
+
+static NSString *const kPackageToken = @"6560cac629e88181bd85310911de1f8c"; 
+
+#endif
